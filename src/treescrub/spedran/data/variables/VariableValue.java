1c937372e2d17c01cdbc6070561d15c6028e2b1a
Treescrub <treescrub@gmail.com>
2022-09-18 20:08:28 -0600

    Added getters

--- src/treescrub/spedran/data/variables/VariableValue.java
+++ src/treescrub/spedran/data/variables/VariableValue.java
@@ -4,6 +4,7 @@ import kong.unirest.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class VariableValue {
     private String label;
@@ -17,5 +18,17 @@ public class VariableValue {
         for (String key : data.getJSONObject("flags").keySet()) {
             flags.put(key, data.getJSONObject("flags").getBoolean(key));
         }
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public Optional<String> getRules() {
+        return Optional.ofNullable(rules);
+    }
+
+    public Map<String, Boolean> getFlags() {
+        return flags;
     }
 }
