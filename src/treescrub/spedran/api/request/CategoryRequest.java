7ffb42cf553a9f96684948c44a44c3056117c5b4
Treescrub <treescrub@gmail.com>
2023-01-31 00:43:45 -0700

    Initial commit

---
+++ src/treescrub/spedran/api/request/CategoryRequest.java
@@ -0,0 +1,18 @@
+package treescrub.spedran.api.request;
+
+import kong.unirest.HttpMethod;
+import kong.unirest.json.JSONObject;
+import treescrub.spedran.data.category.Category;
+
+import java.util.Map;
+
+public class CategoryRequest extends SingleResourceRequest<Category> {
+    protected CategoryRequest(String id) {
+        super(HttpMethod.GET, "categories/{id}", Map.of("id", id));
+    }
+
+    @Override
+    protected Category parse(JSONObject data) {
+        return new Category(data);
+    }
+}
