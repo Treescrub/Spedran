d00c3c9626d9c7e7ece0408e491b2415c120e60f
Treescrub <treescrub@gmail.com>
2023-02-03 09:51:49 -0700

    Initial commit

---
+++ src/treescrub/spedran/api/request/GuestRequest.java
@@ -0,0 +1,18 @@
+package treescrub.spedran.api.request;
+
+import kong.unirest.HttpMethod;
+import kong.unirest.json.JSONObject;
+import treescrub.spedran.data.Guest;
+
+import java.util.Map;
+
+public class GuestRequest extends SingleResourceRequest<Guest> {
+    public GuestRequest(String name) {
+        super(HttpMethod.GET, "guests/{name}", Map.of("name", name));
+    }
+
+    @Override
+    protected Guest parse(JSONObject data) {
+        return new Guest(data);
+    }
+}
