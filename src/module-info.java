ac66277698fc116237329ccf0cdd360feb2bba59
Treescrub <treescrub@gmail.com>
2023-10-27 01:34:13 -0600

    Fix missing module exports for request builders

--- src/module-info.java
+++ src/module-info.java
@@ -10,7 +10,21 @@ module Spedran {
     exports treescrub.spedran.data.category;
     exports treescrub.spedran.data.leaderboard;
     exports treescrub.spedran.api;
-    exports treescrub.spedran.api.request.run;
     exports treescrub.spedran.api.request;
+    exports treescrub.spedran.api.request.category;
+    exports treescrub.spedran.api.request.developer;
+    exports treescrub.spedran.api.request.engine;
+    exports treescrub.spedran.api.request.game;
+    exports treescrub.spedran.api.request.gametype;
+    exports treescrub.spedran.api.request.genre;
+    exports treescrub.spedran.api.request.guest;
+    exports treescrub.spedran.api.request.leaderboard;
+    exports treescrub.spedran.api.request.level;
+    exports treescrub.spedran.api.request.platform;
+    exports treescrub.spedran.api.request.publisher;
+    exports treescrub.spedran.api.request.region;
+    exports treescrub.spedran.api.request.run;
+    exports treescrub.spedran.api.request.series;
     exports treescrub.spedran.api.request.user;
+    exports treescrub.spedran.api.request.variable;
 }
