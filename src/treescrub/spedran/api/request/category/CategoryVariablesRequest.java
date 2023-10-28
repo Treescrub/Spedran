c02d755eed4c1b16e22203c6ca6958ca502ed7e2
Treescrub <treescrub@gmail.com>
2023-10-28 16:39:01 -0600

    Add documentation

--- src/treescrub/spedran/api/request/category/CategoryVariablesRequest.java
+++ src/treescrub/spedran/api/request/category/CategoryVariablesRequest.java
@@ -19,26 +19,55 @@ public class CategoryVariablesRequest extends ResourceCollectionRequest<Variable
         this(category.getId());
     }
 
+    /**
+     * Sorts the returned variables alphanumerically by name.
+     *
+     * @return this object
+     * @see Variable#getName()
+     */
     public CategoryVariablesRequest sortByName() {
         setSortParameter("name");
         return this;
     }
 
+    /**
+     * Sorts the returned variables by mandatory flag.
+     *
+     * @return this object
+     * @see Variable#isMandatory()
+     */
     public CategoryVariablesRequest sortByMandatory() {
         setSortParameter("mandatory");
         return this;
     }
 
+    /**
+     * Sorts the returned variables by user defined flag.
+     *
+     * @return this object
+     * @see Variable#isUserDefined()
+     */
     public CategoryVariablesRequest sortByUserDefined() {
         setSortParameter("user-defined");
         return this;
     }
 
+    /**
+     * Sorts the returned variables by the moderator defined position.
+     *
+     * @return this object
+     */
     public CategoryVariablesRequest sortByPosition() {
         setSortParameter("pos");
         return this;
     }
 
+    /**
+     * Sets the direction to sort the returned variables.
+     *
+     * @param direction the direction to sort, either ascending or descending
+     * @return this object
+     */
     public CategoryVariablesRequest sortDirection(SortDirection direction) {
         setSortDirection(direction);
         return this;
