8300e8fceafa05b34f47f8e33c14091fa86d3390
Treescrub <treescrub@gmail.com>
2024-06-06 02:34:18 -0600

    Fix bad assertion comparisons between incompatible types

--- src/test/java/com/github/treescrub/spedran/game/GameRulesetTest.java
+++ src/test/java/com/github/treescrub/spedran/game/GameRulesetTest.java
@@ -2,6 +2,7 @@ package com.github.treescrub.spedran.game;
 
 import com.github.treescrub.spedran.JSONLoader;
 import com.github.treescrub.spedran.data.game.GameRuleset;
+import com.github.treescrub.spedran.data.run.TimingType;
 import kong.unirest.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
@@ -46,7 +47,7 @@ class GameRulesetTest {
         JSONObject json = JSONLoader.getJsonTestFile("l4d/game/gameruleset");
         GameRuleset ruleset = new GameRuleset(json);
 
-        assertTrue(ruleset.getRunTimes().contains("realtime"));
+        assertTrue(ruleset.getRunTimes().contains(TimingType.REALTIME));
     }
 
     @Test
@@ -54,6 +55,6 @@ class GameRulesetTest {
         JSONObject json = JSONLoader.getJsonTestFile("l4d/game/gameruleset");
         GameRuleset ruleset = new GameRuleset(json);
 
-        assertEquals("realtime", ruleset.getDefaultTime());
+        assertEquals(TimingType.REALTIME, ruleset.getDefaultTime());
     }
 }
