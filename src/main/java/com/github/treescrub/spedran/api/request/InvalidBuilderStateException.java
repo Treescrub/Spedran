e578a1eef35bfda45f12e957f1c4b5a7f0a025d8
Treescrub <treescrub@gmail.com>
2024-06-24 12:35:37 -0600

    Add exception for request builders being in an invalid state

---
+++ src/main/java/com/github/treescrub/spedran/api/request/InvalidBuilderStateException.java
@@ -0,0 +1,7 @@
+package com.github.treescrub.spedran.api.request;
+
+public class InvalidBuilderStateException extends Exception {
+    public InvalidBuilderStateException(String message) {
+        super(message);
+    }
+}
