017fff1b72371f7c3806cb172971c2d5d9f4ec5e
Treescrub <treescrub@gmail.com>
2024-06-24 14:44:01 -0600

    Implement changing run players request

--- src/main/java/com/github/treescrub/spedran/api/request/run/RunPlayersRequest.java
+++ src/main/java/com/github/treescrub/spedran/api/request/run/RunPlayersRequest.java
@@ -1,25 +1,114 @@
 package com.github.treescrub.spedran.api.request.run;
 
-import kong.unirest.HttpMethod;
-import kong.unirest.json.JSONObject;
-import com.github.treescrub.spedran.api.request.SingleResourceRequest;
+import com.github.treescrub.spedran.api.request.InvalidBuilderStateException;
+import com.github.treescrub.spedran.api.request.ModifyResourceRequest;
+import com.github.treescrub.spedran.data.Guest;
 import com.github.treescrub.spedran.data.run.Run;
+import com.github.treescrub.spedran.data.user.User;
+import kong.unirest.HttpMethod;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONElement;
+import kong.unirest.json.JSONObject;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class RunPlayersRequest extends SingleResourceRequest<Run> {
+/**
+ * A builder to set the players that participated in a {@link Run}.
+ * <br>
+ * Players will maintain the order that they were added.
+ * <br>
+ * Duplicate users/guests will be ignored.
+ */
+public class RunPlayersRequest extends ModifyResourceRequest<Run> {
+    private final Map<String, String> runPlayers;
+
     public RunPlayersRequest(String id) {
         super(HttpMethod.PUT, "runs/{id}/players", Map.of("id", id));
+
+        runPlayers = new LinkedHashMap<>();
     }
 
     public RunPlayersRequest(Run run) {
         this(run.getId());
     }
 
-    // TODO: authentication, run players serialization
+    /**
+     * Adds a {@link User} to the players that will be set on the {@link Run}.
+     *
+     * @param user the user to add to the list of players
+     * @return this builder object
+     */
+    public RunPlayersRequest addUser(User user) {
+        return addUser(user.getId());
+    }
+
+    /**
+     * Adds a {@link User} to the players that will be set on the {@link Run}.
+     *
+     * @param userId the ID of the user to add to the list of players
+     * @return this builder object
+     */
+    public RunPlayersRequest addUser(String userId) {
+        runPlayers.put("user", userId);
+
+        return this;
+    }
+
+    /**
+     * Adds a {@link Guest} to the players that will be set on the {@link Run}.
+     *
+     * @param guest the guest to add to the list of players
+     * @return this builder object
+     */
+    public RunPlayersRequest addGuest(Guest guest) {
+        return addGuest(guest.getName());
+    }
+
+    /**
+     * Adds a {@link Guest} to the players that will be set on the {@link Run}.
+     *
+     * @param guestName the name of the guest to add to the list of players
+     * @return this builder object
+     */
+    public RunPlayersRequest addGuest(String guestName) {
+        runPlayers.put("guest", guestName);
+
+        return this;
+    }
 
     @Override
     protected Run parse(JSONObject data) {
         return new Run(data);
+    }
+
+    @Override
+    protected JSONElement buildBody() throws InvalidBuilderStateException {
+        if(runPlayers.isEmpty()) {
+            throw new InvalidBuilderStateException("At least one player must be set");
+        }
+        if(runPlayers.size() > 20) {
+            throw new InvalidBuilderStateException("Exceeded max number of players (20)");
+        }
+
+        JSONArray playersNode = new JSONArray();
+        for(Map.Entry<String, String> entry : runPlayers.entrySet()) {
+            JSONObject playerNode = new JSONObject();
+            playerNode.put("rel", entry.getKey());
+
+            String idKeyName;
+            if(entry.getKey().equals("user")) {
+                idKeyName = "id";
+            } else {
+                idKeyName = "name";
+            }
+
+            playerNode.put(idKeyName, entry.getValue());
+        }
+
+        JSONObject rootNode = new JSONObject();
+        rootNode.put("players", playersNode);
+
+        return rootNode;
     }
 }
