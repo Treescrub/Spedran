793b995adfc72f9772af7eba306cca0e739bd27a
Treescrub <treescrub@gmail.com>
2024-06-23 20:11:45 -0600

    Fix missing constructor

--- src/main/java/com/github/treescrub/spedran/api/request/ModifyResourceRequest.java
+++ src/main/java/com/github/treescrub/spedran/api/request/ModifyResourceRequest.java
@@ -3,11 +3,16 @@ package com.github.treescrub.spedran.api.request;
 import kong.unirest.HttpMethod;
 import kong.unirest.json.JSONElement;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public abstract class ModifyResourceRequest<T> extends SingleResourceRequest<T> {
     protected ModifyResourceRequest(HttpMethod method, String url) {
         super(method, url);
+    }
+
+    public ModifyResourceRequest(HttpMethod method, String url, Map<String, Object> routeParameters) {
+        super(method, url, routeParameters);
     }
 
     protected abstract JSONElement buildBody();
