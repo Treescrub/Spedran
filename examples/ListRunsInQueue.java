90010f3e7f3bf0059e2d469851533c9e1f458d6c
Treescrub <treescrub@gmail.com>
2024-07-05 21:54:59 -0600

    Add example to get runs in verification queue

---
+++ examples/ListRunsInQueue.java
@@ -0,0 +1,32 @@
+import com.github.treescrub.spedran.Spedran;
+import com.github.treescrub.spedran.data.Game;
+import com.github.treescrub.spedran.data.Run;
+import com.github.treescrub.spedran.data.SubmissionStatus;
+
+import java.time.Instant;
+import java.util.List;
+
+class ListRunsInQueue {
+    public static void main(String[] args) {
+        String gameAbbreviation = "celeste";
+
+        // Get the game using the abbreviation
+        Game game = Spedran.getGame(gameAbbreviation).join();
+
+        // Get a list of new runs, sorted by the submission time
+        List<Run> runsInQueue = Spedran.getRuns()
+                .game(game)
+                .status(SubmissionStatus.NEW)
+                .sortBySubmissionDate()
+                .complete()
+                .join();
+
+        // Print the link to each run in the game's verification queue
+        for(Run run : runsInQueue) {
+            System.out.println(run.getWeblink());
+        }
+
+        // Shut down the library to allow the application to exit
+        Spedran.shutDown();
+    }
+}
---
+++ examples/ListRunsInQueueAsync.java
@@ -0,0 +1,27 @@
+import com.github.treescrub.spedran.Spedran;
+import com.github.treescrub.spedran.data.Run;
+import com.github.treescrub.spedran.data.SubmissionStatus;
+
+class ListRunsInQueueAsync {
+    public static void main(String[] args) {
+        String gameAbbreviation = "celeste";
+
+        // Get the game using the abbreviation
+        Spedran.getGame(gameAbbreviation).thenAcceptAsync(game -> {
+            // Get a list of new runs, sorted by the submission time
+            Spedran.getRuns()
+                    .game(game)
+                    .status(SubmissionStatus.NEW)
+                    .sortBySubmissionDate()
+                    .complete().thenAccept(runsInQueue -> {
+                        // Print the link to each run in the game's verification queue
+                        for(Run run : runsInQueue) {
+                            System.out.println(run.getWeblink());
+                        }
+
+                        // Shut down the library to allow the application to exit
+                        Spedran.shutDown();
+                    });
+        });
+    }
+}
